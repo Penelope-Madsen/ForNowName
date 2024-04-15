d6306add36209922e0b929de55983cea18b0b20a
9593171 <penelope.madsen@gmail.com>
2024-04-15 09:06:34 -0600

    4/15

--- src/Card.py
+++ src/Card.py
@@ -5,15 +5,16 @@ screen = pygame.display.set_mode(size)
 pygame.init()
 
 class Card:
-    def __init__(self, tcolor, text, font, bcolor, crewe, cargoe, fuele, lawe):
+    def __init__(self, tcolor, text, font, bcolor):
+        # crewe, cargoe, fuele, lawe
         self.tcolor = tcolor
         self.text = text
         self.font = font
         self.bcolor = bcolor
-        self.crewe = crewe
-        self.cargoe = cargoe
-        self.fuele = fuele
-        self.lawe = lawe
+        # self.crewe = crewe
+        # self.cargoe = cargoe
+        # self.fuele = fuele
+        # self.lawe = lawe
 
     # card = pygame.Rect(0, height / 2 - 200, 320, 320)
     # card.center = (width / 2, height / 2)
@@ -28,8 +29,8 @@ class Card:
             stateRect.center = (width / 2, height / 2 - 175 + 20 * i)
             screen.blit(state, stateRect)
 
-    def hover(self):
-        if
+    # def hover(self):
+    #     if
 
     # def left(self):
     #     mouse.get_position()
