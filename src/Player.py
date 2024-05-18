092f1c23fc52fcdd2028cc9d94ba4d81d2a59a02
Penny Madsen <penelope.madsen@gmail.com>
2024-05-17 23:54:31 -0600

    5/17

--- src/Player.py
+++ src/Player.py
@@ -2,16 +2,12 @@ import random
 
 
 class Player:
-    def __init__(self, crew1, fuel1, cargo1, law1, name1, goal1, prog1, clicked1, up1, nextcard1):
+    def __init__(self, crew1, fuel1, cargo1, law1, name1, up1, nextcard1):
         self.crew = crew1
         self.fuel = fuel1
         self.cargo = cargo1
         self.law = law1
         self.name = name1
-        # self.progress = progress1
-        self.goal = goal1
-        self.prog = prog1
-        self.clicked = clicked1
         self.up = up1
         self.nextCard = nextcard1
 
@@ -29,4 +25,4 @@ up = True
 nextCard = 'welc'  # default value
 ships = ["Andromeda", "Argo", "Starbuster 5000", "Galaxy Devourer 4000"]
 name = ships[random.randint(0, len(ships)-1)]
-p1 = Player(crew, fuel, cargo, law, name, goal, prog, clicked, up, nextCard)
+p1 = Player(crew, fuel, cargo, law, name, up, nextCard)
